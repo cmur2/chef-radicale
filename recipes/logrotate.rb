9b48237f04f44f26a64c96593fe5706f04428301
cn <cn@mycrobase.de>
2013-10-08 21:25:31 +0200

    Remove unused conditional and fix bug in logrotate

--- recipes/logrotate.rb
+++ recipes/logrotate.rb
@@ -2,15 +2,13 @@
 begin
   include_recipe "logrotate"
 
-  if log
-    logrotate_app "radicale" do
-      cookbook "logrotate"
-      path "/var/log/radicale.log"
-      options ["missingok", "compress", "copytruncate"]
-      frequency "weekly"
-      create "644 radicale radicale"
-      rotate 4
-    end
+  logrotate_app "radicale" do
+    cookbook "logrotate"
+    path "/var/log/radicale.log"
+    options ["missingok", "compress", "copytruncate"]
+    frequency "weekly"
+    create "644 radicale radicale"
+    rotate 4
   end
 rescue
   Chef::Log.error "radicale::logrotate requires the logrotate cookbook!"
