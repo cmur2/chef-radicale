7da68c4a1cec86bb08cef30ab36a4aebd9b7938a
cn <cn@mycrobase.de>
2013-10-08 22:45:36 +0200

    Restart radicale on logrotate

--- recipes/logrotate.rb
+++ recipes/logrotate.rb
@@ -7,8 +7,9 @@ begin
     path "/var/log/radicale.log"
     options ["missingok", "compress", "copytruncate"]
     frequency "weekly"
-    create "644 radicale radicale"
+    create "640 radicale radicale"
     rotate 4
+    postrotate "invoke-rc.d radicale restart > /dev/null"
   end
 rescue
   Chef::Log.error "radicale::logrotate requires the logrotate cookbook!"
